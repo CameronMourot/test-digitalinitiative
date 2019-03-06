12163a0daa983bc6b862a9d8de4f8be03ca7b909
CameronMourot <cameron_mourot@aol.com>
2019-03-06 21:38:30 +0100

    [~] WIP: Header home-page w/ icomoon

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".main").onepage_scroll({
+    $("#main").onepage_scroll({
         sectionContainer: "section", // sectionContainer accepts any kind of selector in case you don't want to use section
         easing: "ease", // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
