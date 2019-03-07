43e6e70625b5d357a69fc948fa2364a2b457aee8
Cameron Mourot <cmourot@axome.com>
2019-03-07 13:59:57 +0100

    [~] Add mmmenu
     - Maybe just add overlay for menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,5 +15,14 @@ $(document).ready(function() {
         // the browser's width is less than 600, the fallback will kick in.
         direction: "vertical" // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
     });
+
+    $("#my-menu").mmenu({
+        // slidingSubmenus: false
+    }, {
+        // configuration
+        offCanvas: {
+            pageNodetype: "main"
+        }
+    });
 });
 console.log('fef');
