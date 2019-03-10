43d34348fcba4376fe9426fc42d0f164a81c7fd9
CameronMourot <cameron_mourot@aol.com>
2019-03-10 17:26:49 +0100

    [*] Disable onepage scroll mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
         afterMove: function(index) {}, // This option accepts a callback function. The function will be called after the page moves.
         loop: false, // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
         keyboard: true, // You can activate the keyboard controls
-        responsiveFallback: false, // You can fallback to normal page scroll by defining the width of the browser in which
+        responsiveFallback: 768, // You can fallback to normal page scroll by defining the width of the browser in which
         // you want the responsive fallback to be triggered. For example, set this to 600 and whenever
         // the browser's width is less than 600, the fallback will kick in.
         direction: "vertical" // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
