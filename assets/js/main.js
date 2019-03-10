f220a57b3eea5486e5f9cd159b082e583727ef49
CameronMourot <cameron_mourot@aol.com>
2019-03-10 15:27:34 +0100

    [+] Change order pages + page 6 & 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,15 +24,21 @@ $(document).ready(function() {
         direction: "vertical" // You can now define the direction of the One Page Scroll animation. Options available are "vertical" and "horizontal". The default value is "vertical".  
     });
 
-    var classes = ['bg-1', 'bg-2', 'bg-3'];
-    var i = 1;
-    var minus = 0;
-    $('.first-page').addClass('bg-1');
+
+    // Background first page
+    var $classes = ['bg-1', 'bg-2', 'bg-3'];
+    var $i = 1;
+    var $minus = 0;
+    var $firstpage = $('#first-page');
+
+    $firstpage.addClass('bg-1');
     setInterval(function() {
-        $('.first-page').addClass(classes[i++ % classes.length]);
-        $('.first-page').removeClass(classes[minus++ % classes.length]);
+        $firstpage.addClass($classes[$i++ % $classes.length]);
+        $firstpage.removeClass($classes[$minus++ % $classes.length]);
     }, 5000);
 
+
+    // Burger menu
     $("#burger").click(function check() {
         $(this).toggleClass("burgeractive");
     });
@@ -49,6 +55,8 @@ $(document).ready(function() {
         $("#burger").removeClass("burgeractive");
     });
 
+
+    // Panels second page
     $(".panel").hover(function() {
             $(this).addClass('panel-hovered');
 
@@ -61,13 +69,15 @@ $(document).ready(function() {
             // $('.panel-content-added').slideUp(400);
         });
 
+
+    // Slider third page
     $('#slider-wrapper .row').slick({
         infinite: true,
         slidesToShow: 2,
         slidesToScroll: 1
     });
 
-    // masonry 
+    // Masonry sixth page
     var $grid = $('.gallery-wrapper').masonry({
         itemSelector: '.grid-item',
         columnWidth: '.grid-sizer',
@@ -78,6 +88,25 @@ $(document).ready(function() {
 
     $grid.imagesLoaded().progress(function() {
         $grid.masonry();
+    });
+
+
+    // Hovering fourth page
+    var $fourth = $('#fourth-page');
+    $(".hover-element-one").hover(function() {
+        $fourth.addClass('bg-1');
+        $fourth.removeClass('bg-2');
+        $fourth.removeClass('bg-3');
+    });
+    $(".hover-element-two").hover(function() {
+        $fourth.addClass('bg-2');
+        $fourth.removeClass('bg-1');
+        $fourth.removeClass('bg-3');
+    });
+    $(".hover-element-three").hover(function() {
+        $fourth.addClass('bg-3');
+        $fourth.removeClass('bg-2');
+        $fourth.removeClass('bg-1');
     });
 
 });
